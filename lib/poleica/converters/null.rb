12b929c1624944e98101718e4549562111f907ac
Antoine Lyset <antoinelyset+github@gmail.com>
2013-06-05 19:34:32 +0200

    Null Pattern removed (will be added if needed)

--- lib/poleica/converters/null.rb
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-module Poleica
-  module Converters
-    class Null
-
-    end # class Null
-  end # module Converters
-end # module Poleica
--- lib/poleica/types/null.rb
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-module Poleica
-  module Types
-    class Null
-      def initialize(file_path)
-      end
-    end # class Null
-  end # module Types
-end # module Poleica
