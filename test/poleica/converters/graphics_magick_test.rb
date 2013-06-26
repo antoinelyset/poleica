0ed0cc163d239d29464f51908ffc7629af13a828
Antoine Lyset <antoinelyset+github@gmail.com>
2013-06-26 12:32:49 +0200

    DRY gm test

--- test/poleica/converters/graphics_magick_test.rb
+++ test/poleica/converters/graphics_magick_test.rb
@@ -2,12 +2,35 @@
 require 'test_helper'
 # Test the GraphicsMagick Converter Module
 class GraphicsMagickTest < Minitest::Test
-  def test_to_png
-    pdf_polei = Poleica::Polei.new("#{Support.support_path}/files/example.pdf")
-    data   = File.read(pdf_polei.path)
-    md5    = Digest::MD5.new
-    digest = md5.hexdigest(data)
-    expected_path = "#{Support.support_path}/files/example-#{digest}.png"
-    assert_equal(expected_path, pdf_polei.to_png)
+
+  PDF_PATH = "#{Support.support_path}/files/example.pdf"
+
+  def setup
+    clean_png_file
+  end
+
+  def test_to_png_returned_path
+    returned_path = pdf_polei.to_png
+    expected_path = Support.expected_converted_path(PDF_PATH, :png)
+    assert_equal(expected_path, returned_path)
+    clean_png_file
+  end
+
+  def test_to_png_create_a_file
+    returned_path = pdf_polei.to_png
+    assert(File.exists?(returned_path))
+    clean_png_file
+  end
+
+  private
+
+  def clean_png_file
+    if File.exists?(Support.expected_converted_path(PDF_PATH, :png))
+      File.delete(Support.expected_converted_path(PDF_PATH, :png))
+    end
+  end
+
+  def pdf_polei
+    Poleica::Polei.new("#{Support.support_path}/files/example.pdf")
   end
 end # class GraphicsMagickTest
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,10 +8,21 @@ end
 gem     'minitest'
 require 'minitest/autorun'
 require 'poleica'
+
 # The Support Module contains support methods for the tests
 module Support
   module_function
   def support_path
     @support_path ||= File.expand_path('../support', __FILE__)
+  end
+
+  def expected_converted_path(
+    original_file_path  = "#{Support.support_path}/files/example.pdf",
+    converted_extension = :png
+    )
+    data   = File.read(original_file_path)
+    md5    = Digest::MD5.new
+    digest = md5.hexdigest(data)
+    "#{Support.support_path}/files/example-#{digest}.#{converted_extension}"
   end
 end
