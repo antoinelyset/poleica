132c447a80c77413f87768712ef74e04c8e574e1
Antoine Lyset <antoinelyset+github@gmail.com>
2013-09-06 15:14:07 +0200

    Update travis YEEEA

--- .travis/init_libreoffice.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/usr/lib/libreoffice/program/soffice.bin --nologo --headless --invisible --norestore --nolockcheck --convert-to pdf test/support/files/example.doc
-rm example.pdf
-exit 0
